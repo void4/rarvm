a64eefbf9601801b5783683caa4556030b8b69db
void4 <void4@noreply.github.com>
2022-11-04 16:37:04 +0100

    Use argparse for nicer calling

--- src/run.py
+++ src/run.py
@@ -1,13 +1,23 @@
 import sys
 sys.dont_write_bytecode = True
+from argparse import ArgumentParser
 
 from compiler.compiler import compile
 from shell import entry_point
 
-path = "bin.b"
+parser = ArgumentParser(
+    prog="rarVM",
+    description= "resource aware, recursively sandboxable virtual machine"
+)
 
-if len(sys.argv) < 2:
-    print("Missing <source>")
+parser.add_argument("filename", default=None)
+parser.add_argument("--debug", default=False, action="store_true")
+parser.add_argument("--binpath", default="bin.b")
+
+args = parser.parse_args()
+
+if args.filename is None:
+    print("Missing <source>.et")
     exit(1)
 
 with open(sys.argv[1], "r") as f:
@@ -23,12 +33,8 @@ change frame layout, put pointers at the start, dont use arealen as often
 
 """
 
-binary.write(path)
-
-if "debug" in sys.argv:
-    debug = True
-else:
-    debug = False
+if args.binpath:#TODO: default: none?
+    binary.write(args.binpath)
 
 #entry_point(["main", path, 1000000000, 1000000000, debug])
-entry_point(binary.data, 1000000000, 1000000000, debug, None)
+entry_point(binary.data, 1000000000, 1000000000, args.debug, None)
