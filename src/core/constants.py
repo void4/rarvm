3c0a9d75218010024dbf78f660fcb85a7f6bcc7f
void4 <void4@noreply.github.com>
2022-11-04 16:36:38 +0100

    Document constants a tiny bit better

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,20 +1,32 @@
+# Byte size in bits
 BYTESIZE = 8
+# Size of a word in bits
 WORDSIZE = 8*BYTESIZE
+# 1 + maximum value of a word
 WMAX = 2**WORDSIZE
+# Maximum value of a word
 WMASK = WMAX-1
 
-STATUS, REC, GAS, MEM, IP = range(5)
+# Process fields
 HEAD, STACK, MAP, MEMORY = range(4)
+
+# Header fields
+STATUS, REC, GAS, MEM, IP = range(5)
+
+# Offsets of fields in the serialized format
 F_STATUS, F_REC, F_GAS, F_MEM, F_IP, F_LENSTACK, F_LENMAP, F_LENMEMORY, F_STACK, F_MAP, F_MEMORY = range(11)
 
 NORMAL, FROZEN, VOLHALT, VOLRETURN, VOLYIELD, OOG, OOC, OOS, OOM, OOB, UOC, RECURSE, IIN = range(13)
 #STATI = ["NORMAL", "FROZEN", "VOLHALT", "VOLRETURN", "VOLYIELD", "OUTOFGAS", "OUTOFCODE", "OUTOFSTACK", "OUTOFMEMORY", "OUTOFBOUNDS", "UNKNOWNCODE", "RUN"]
 STATI = ["NOR", "FRZ", "HLT", "RET", "YLD", "OOG", "OOC", "OOS", "OOM", "OOB", "UOC", "REC"]
 
+# Instruction IDs
 HALT, RETURN, YIELD, RUN, JUMP, JUMPR, JZ, JZR, PUSH, POP, DUP, FLIP, KEYSET, KEYHAS, KEYGET, KEYDEL, STACKLEN, MEMORYLEN, AREALEN, READ, WRITE, AREA, DEAREA, ALLOC, DEALLOC, ADD, SUB, NOT, MUL, DIV, MOD, SHA256, ECVERIFY, ROT, ROT2, CMP, HEADER = range(37)
 
+# Instruction names as usable in the assembler
 INSTR = ["HALT", "RETURN", "YIELD", "RUN", "JUMP", "JUMPR", "JZ", "JZR", "PUSH", "POP", "DUP", "FLIP", "KEYSET", "KEYHAS", "KEYGET", "KEYDEL", "STACKLEN", "MEMORYLEN", "AREALEN", "READ", "WRITE", "AREA", "DEAREA", "ALLOC", "DEALLOC", "ADD", "SUB", "NOT", "MUL", "DIV", "MOD", "SHA256", "ECVERIFY", "ROT", "ROT2", "CMP", "HEADER"]
 
+# Instruction properties and requirements
 REQS = [
     # Name, Instruction length, Required Stack Size, Stack effect, Gas cost
     [1,0,0,1],
